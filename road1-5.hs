312b8f3f93eabb4c8815a81e21686c32fa74c429
comatose <boncheol.gu@gmail.com>
2011-01-23 22:15:53 +0900

    completed Chapter 1

--- road1-5.hs
+++ road1-5.hs
@@ -52,3 +52,66 @@ substrings' :: String -> String -> Bool
 substrings' [] _ = True
 substrings' _ [] = False
 substrings' xs ys = prefix' xs ys || substrings' xs (tail ys)
+
+blowup :: String -> String
+blowup = blowup' 1
+
+blowup' _ [] = []
+blowup' n (x:xs) = (replicate' n x) ++ blowup' (n + 1) xs 
+
+replicate' :: Int -> a -> [a]
+replicate' 0 _ = []
+replicate' n c = c : replicate' (n - 1) c
+
+factors :: Integer -> [Integer]
+factors 1 = []
+factors n = p : factors n'
+    where
+        p = ld n
+        n' = n `div` p
+
+map' :: (a -> b) -> [a] -> [b]
+map' f (x:xs) = f x : map' f xs
+map' _ [] = []
+
+lengths :: [[a]] -> [Int]
+lengths = map length
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' f (x:xs)
+    | f x = x : filter' f xs
+    | otherwise = filter' f xs
+filter' _ [] = []
+
+ld :: Integer -> Integer
+ld n = ldf 2 n
+
+ldf :: Integer -> Integer -> Integer
+ldf i n
+    | i^2 > n = n
+    | n `rem` i == 0 = i
+    | otherwise = ldf (i + 1) n
+
+prime0 :: Integer -> Bool
+prime0 n = ld n == n
+
+primes0 :: [Integer]
+primes0 = 2 : filter prime0 [3..]
+
+ldp :: Integer -> Integer
+ldp = ldpf primes1
+
+ldpf :: [Integer] -> Integer -> Integer
+ldpf (p:ps) n
+    | rem n p == 0 = p
+    | p^2 > n = n
+    | otherwise = ldpf ps n
+
+primes1 :: [Integer]
+primes1 = 2 : filter prime [3..]
+
+prime :: Integer -> Bool
+prime n
+    | n < 1 = error "not a positive integer"
+    | n == 1 = False
+    | otherwise = ldp n == n
